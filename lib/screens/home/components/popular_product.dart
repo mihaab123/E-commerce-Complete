3881ca412b9bc8a4ce23910040e8f9072b61e524
Alexander Mihailov <miha.ab.2017@gmail.com>
2022-03-13 22:45:08 +0200

    show products

--- lib/screens/home/components/popular_product.dart
+++ lib/screens/home/components/popular_product.dart
@@ -1,41 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:shop_app/components/product_card.dart';
+import 'package:shop_app/controllers/product_controller.dart';
 import 'package:shop_app/models/Product.dart';
-import 'package:shop_app/utils.dart';
 
 import '../../../size_config.dart';
 import 'section_title.dart';
 
 class PopularProducts extends StatelessWidget {
+  final AllProducts _productController = Get.find<AllProducts>();
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding:
-              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-          child: SectionTitle(title: "HS_popular_products".tr, press: () {}),
-        ),
-        SizedBox(height: getProportionateScreenWidth(20)),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              ...List.generate(
-                demoProducts.length,
-                (index) {
-                  if (demoProducts[index].isPopular)
-                    return ProductCard(product: demoProducts[index]);
-
-                  return SizedBox
-                      .shrink(); // here by default width and height is 0
-                },
-              ),
-              SizedBox(width: getProportionateScreenWidth(20)),
-            ],
+    return Obx(() {
+      return Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(20)),
+            child: SectionTitle(title: "HS_popular_products".tr, press: () {}),
           ),
-        )
-      ],
-    );
+          SizedBox(height: getProportionateScreenWidth(20)),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                ...List.generate(
+                  _productController.products.length,
+                  (index) {
+                    if (_productController.products[index].isPopular)
+                      return ProductCard(
+                          product: _productController.products[index]);
+
+                    return SizedBox
+                        .shrink(); // here by default width and height is 0
+                  },
+                ),
+                SizedBox(width: getProportionateScreenWidth(20)),
+              ],
+            ),
+          )
+        ],
+      );
+    });
   }
 }
